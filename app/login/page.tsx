4b8401705cc57f73e0db60cbce351f5e56954ef1
rjmasache <royerjmasache@gmail.com>
2025-08-06 17:29:17 -0500

    feat: setup initial config for authentication and install zod

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,33 @@
+import { signIn } from "@/auth";
+
 export default function Page() {
-    return <h1>Login Page</h1>;
+    return (
+        <form
+            action={async (formData) => {
+                "use server";
+                await signIn("credentials", formData);
+            }}
+            className="flex flex-col items-center justify-center gap-5"
+        >
+            <label>
+                Email
+                <input
+                    name="email"
+                    type="email"
+                    className="m-5 rounded-sm border-[0.5]"
+                />
+            </label>
+            <label>
+                Password
+                <input
+                    name="password"
+                    type="password"
+                    className="m-5 rounded-sm border-[0.5]"
+                />
+            </label>
+            <button className="rounded-sm border-[0.5] bg-black p-2 text-sm text-white">
+                Sign In
+            </button>
+        </form>
+    );
 }

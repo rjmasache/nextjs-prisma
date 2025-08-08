85ef7b202674e4acbbe6986192ea89ce90eb6381
rjmasache <royerjmasache@gmail.com>
2025-08-08 00:32:35 -0500

    feat: update auth logic - app/utils/actions.ts: create action to connect auth logic with llogin form - auth.ts: delete credentials field - app/login.tsx: add useActionState to manage auth logic - app/dashboard-designer/page.tsx: add logout functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,27 @@
-import { signIn } from "@/auth";
+"use client";
+
+import { useActionState } from "react";
+import { authenticate } from "@/app/utils/actions";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+    const searchParams = useSearchParams();
+    const callbackUrl =
+        searchParams.get("callbackUrl") || "/dashboard-designer";
+    const [errorMessage, formAction, isPending] = useActionState(
+        authenticate,
+        undefined,
+    );
+
     return (
         <form
-            action={async (formData) => {
-                "use server";
-                await signIn("credentials", formData);
-            }}
+            action={formAction}
             className="flex flex-col items-center justify-center gap-5"
         >
             <label>
                 Email
                 <input
+                    id="email"
                     name="email"
                     type="email"
                     className="m-5 rounded-sm border-[0.5]"
@@ -20,17 +30,25 @@ export default function Page() {
             <label>
                 Password
                 <input
+                    id="password"
                     name="password"
                     type="password"
                     className="m-5 rounded-sm border-[0.5]"
                 />
             </label>
+            <input type="hidden" name="redirectTo" value={callbackUrl} />
             <button
                 type="submit"
                 className="rounded-sm border-[0.5] bg-black p-2 text-sm text-white"
+                aria-disabled={isPending}
             >
                 Sign In
             </button>
+            {errorMessage && (
+                <>
+                    <p className="text-sm text-red-500">{errorMessage}</p>
+                </>
+            )}
         </form>
     );
 }
--- auth.ts
+++ auth.ts
@@ -2,31 +2,14 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { authConfig } from "@/auth.config";
 import { signInSchema } from "@/lib/zod";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
-// Your own logic for dealing with plaintext password strings; be careful!
-// import { saltAndHashPassword } from "@/utils/password";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     ...authConfig,
-    adapter: PrismaAdapter(prisma),
     providers: [
         Credentials({
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            credentials: {
-                email: {
-                    type: "email",
-                    label: "Email",
-                },
-                password: {
-                    type: "password",
-                    label: "Password",
-                },
-            },
-
-            authorize: async (credentials) => {
+            async authorize(credentials) {
                 const parsedCredentials =
                     await signInSchema.safeParseAsync(credentials);
 
@@ -53,6 +36,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                         return user;
                     }
                 }
+
+                console.log("Invalid credentials");
 
                 return null;
             },

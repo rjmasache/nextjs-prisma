3639b1ca07b1c8050962f8be4bf11022dab2c9ed
rjmasache <royerjmasache@gmail.com>
2025-08-08 00:45:34 -0500

    fix: create login form component and wrap with Suspense in login page

---
+++ app/components/login-form.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useActionState } from "react";
+import { authenticate } from "@/app/utils/actions";
+import { useSearchParams } from "next/navigation";
+
+export default function LoginForm() {
+    const searchParams = useSearchParams();
+    const callbackUrl =
+        searchParams.get("callbackUrl") || "/dashboard-designer";
+    const [errorMessage, formAction, isPending] = useActionState(
+        authenticate,
+        undefined,
+    );
+
+    return (
+        <form
+            action={formAction}
+            className="flex flex-col items-center justify-center gap-5"
+        >
+            <label>
+                Email
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    className="m-5 rounded-sm border-[0.5]"
+                />
+            </label>
+            <label>
+                Password
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="m-5 rounded-sm border-[0.5]"
+                />
+            </label>
+            <input type="hidden" name="redirectTo" value={callbackUrl} />
+            <button
+                type="submit"
+                className="rounded-sm border-[0.5] bg-black p-2 text-sm text-white"
+                aria-disabled={isPending}
+            >
+                Sign In
+            </button>
+            {errorMessage && (
+                <>
+                    <p className="text-sm text-red-500">{errorMessage}</p>
+                </>
+            )}
+        </form>
+    );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,56 +1,13 @@
-"use client";
-
-import { Suspense, useActionState } from "react";
-import { authenticate } from "@/app/utils/actions";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import LoginForm from "@/app/components/login-form";
 
 export default function Page() {
-    const searchParams = useSearchParams();
-    const callbackUrl =
-        searchParams.get("callbackUrl") || "/dashboard-designer";
-    const [errorMessage, formAction, isPending] = useActionState(
-        authenticate,
-        undefined,
-    );
-
     return (
-        <Suspense>
-            <form
-                action={formAction}
-                className="flex flex-col items-center justify-center gap-5"
-            >
-                <label>
-                    Email
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        className="m-5 rounded-sm border-[0.5]"
-                    />
-                </label>
-                <label>
-                    Password
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        className="m-5 rounded-sm border-[0.5]"
-                    />
-                </label>
-                <input type="hidden" name="redirectTo" value={callbackUrl} />
-                <button
-                    type="submit"
-                    className="rounded-sm border-[0.5] bg-black p-2 text-sm text-white"
-                    aria-disabled={isPending}
-                >
-                    Sign In
-                </button>
-                {errorMessage && (
-                    <>
-                        <p className="text-sm text-red-500">{errorMessage}</p>
-                    </>
-                )}
-            </form>
-        </Suspense>
+        <>
+            <div>Welcome</div>
+            <Suspense>
+                <LoginForm />
+            </Suspense>
+        </>
     );
 }
